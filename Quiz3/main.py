6e91ca3778b12aa5cba3c57f5695202624ad7952
TheOmnipotentPotato <jacobnickodemus@Jacobs-MacBook-Pro.local>
2023-11-18 13:03:39 -0700

    Commented the solution code

--- Quiz3/main.py
+++ Quiz3/main.py
@@ -3,9 +3,7 @@ import pen
 def new_pen():
     return pen.Pen()
 
-
-
-
+#proper main function area
 if __name__ == "__main__":
     p = new_pen()
     print(p.check_ink())
--- Quiz3/pen.py
+++ Quiz3/pen.py
@@ -1,4 +1,4 @@
-
+#Contains Pen Class and Mehtods
 
 class Pen:
     def __init__(self):
@@ -14,6 +14,10 @@ class Pen:
         status: str = ""
         ink_buffer: int = self.mInk
         for i in range(0,5):
+            #rather nasty one line solution to the string concat
+            #it could be more elegantly written but I think this
+            #minimizes the number of control flow branches to be
+            #checked at runtime, not that performace is a big issue
             status += ("#"*(ink_buffer>0) or ".")
             ink_buffer -= 1
         return "[" + status + "]"
